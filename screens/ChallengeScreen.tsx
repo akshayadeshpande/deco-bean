e7bfc0477eecd4248453e20d236d1c29e5d43759
David Riddell <davidriddell27@outlook.com>
2020-08-19 17:40:42 +1000

    Added button that changes states(count at this stage), will be able to be swapped out for words and pictures once we have decided what to add

--- screens/ChallengeScreen.tsx
+++ screens/ChallengeScreen.tsx
@@ -1,21 +1,64 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
+import { useState, useEffect } from 'react';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function ChallengeScreen() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Challenge Mode</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.text}>Challenge Mode Game Screen</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Challenge Mode</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </View>
+      <View style={styles.CMContainer}>
+        <Text>{count}</Text>
+        <Button title="ClickMe" onPress={() => setCount(count + 1)}/> 
+      </View>
     </View>
   );
 }
 
+/*
+export default function ChallengeScreen() {
+  var i = 0;
+  const testChallenge = ["This", "is", "a", "Test"];
+  var word = testChallenge[i];
+  
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Challenge Mode</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </View>
+      <View style={styles.CMContainer}>
+        <Text style={styles.text}>
+          {word}
+          </Text>
+          <Button
+            title="New Word"
+            onPress={() => i = i+ 1}
+            />
+      </View>
+    </View>
+  );
+}
+*/
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  CMContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
@@ -33,3 +76,30 @@ const styles = StyleSheet.create({
     padding: 20,
   }
 });
+
+
+/*
+export default function ChallengeScreen() {
+  var i = 0;
+  const testChallenge = ["This", "is", "a", "Test"];
+  var word = testChallenge[i];
+  
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Challenge Mode</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </View>
+      <View style={styles.CMContainer}>
+        <Text style={styles.text}>
+          {word}
+          </Text>
+          <Button
+            title="New Word"
+            onPress={() => i = i+ 1}
+            />
+      </View>
+    </View>
+  );
+}
+*/
