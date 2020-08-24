d988a5860583e06f13fd1fea40212d4dd3c3795e
David Riddell <davidriddell27@outlook.com>
2020-08-24 11:10:51 +1000

    Used state hooks on challenge screen so it can now be swapped out for things in DB

--- screens/ChallengeScreen.tsx
+++ screens/ChallengeScreen.tsx
@@ -1,32 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, Button } from 'react-native';
-import { useState, useEffect } from 'react';
+import { StyleSheet, Button, Image, Platform } from 'react-native';
+import { useState, useEffect, Component } from 'react';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function ChallengeScreen() {
-  const [count, setCount] = useState(0);
-  return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Challenge Mode</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      </View>
-      <View style={styles.CMContainer}>
-        <Text>{count}</Text>
-        <Button title="ClickMe" onPress={() => setCount(count + 1)}/> 
-      </View>
-    </View>
-  );
-}
 
-/*
 export default function ChallengeScreen() {
-  var i = 0;
-  const testChallenge = ["This", "is", "a", "Test"];
-  var word = testChallenge[i];
-  
+  const [img, newImg] = useState('https://reactnativecode.com/wp-content/uploads/2018/02/motorcycle.jpg');
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -34,18 +15,18 @@ export default function ChallengeScreen() {
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       </View>
       <View style={styles.CMContainer}>
-        <Text style={styles.text}>
-          {word}
-          </Text>
-          <Button
-            title="New Word"
-            onPress={() => i = i+ 1}
-            />
+      <Image 
+              source = {{ uri: img}}
+ 
+              style = {styles.imageStyle} />
+
+        <Button title="Click Here To Load Image From Different Source" onPress={
+          () => newImg('https://reactnativecode.com/wp-content/uploads/2017/10/Guitar.jpg')
+        } />
       </View>
     </View>
   );
 }
-*/
 
 const styles = StyleSheet.create({
   container: {
@@ -74,7 +55,12 @@ const styles = StyleSheet.create({
   },
   text: {
     padding: 20,
-  }
+  },
+  imageStyle:{
+    width: 200, 
+    height: 300, 
+    resizeMode: 'center'
+   }
 });
 
 
