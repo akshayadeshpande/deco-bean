c05b5e853a3b36520c73a412136d238c50368034
Akshay Deshpande <akshay.deshpande98@gmail.com>
2020-09-07 00:40:12 +1000

    start of backend functions

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Cloud Firestore under the path /messages/:documentId/original
+exports.getWords = functions.https.onRequest(async (req, res) => {
+    try{
+
+        let result = await admin.firestore().collection('WordData').get(); //TODO: add query if needed
+        let response = [];
+      
+        result.forEach(doc => {
+          response.push(doc.data());
+        });
+      
+        return res.send( { "collection" : response} );
+      
+    }catch(err){
+        res.status(500).send(err);
+    }
+  });
