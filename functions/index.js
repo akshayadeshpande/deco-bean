6c81a11ffe64decd077ba3371e2652d754a81655
Akshay Deshpande <akshay.deshpande98@gmail.com>
2020-09-09 21:39:19 +1000

    backend functions primitive

--- functions/index.js
+++ functions/index.js
@@ -26,3 +26,43 @@ exports.getWords = functions.https.onRequest(async (req, res) => {
         res.status(500).send(err);
     }
   });
+
+
+exports.startChallenge = functions.https.onRequest(async (req, res) => {
+    try{
+
+        let result = await admin.firestore().collection('WordData').get(); //TODO: add query if needed
+        let response = [];
+      
+        result.forEach(doc => {
+          response.push(doc.data());
+        });
+      
+        return res.send( { "collection" : response} );
+      
+    }catch(err){
+        res.status(500).send(err);
+    }
+});
+
+exports.getUsers = functions.https.onRequest(async (req, res) => {
+    try{
+
+        let result = await admin.firestore().collection('User').get(); //TODO: add query if needed
+        let response = [];
+      
+        result.forEach(doc => {
+          response.push(doc.data());
+        });
+      
+        return res.send( { "collection" : response} );
+      
+    }catch(err){
+        res.status(500).send(err);
+    }
+  });
+
+
+
+
+
