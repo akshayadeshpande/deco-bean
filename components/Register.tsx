08a947daf2dfd8a1fe4600196d11f59b3941f9d6
Akshay Deshpande <akshay.deshpande98@gmail.com>
2020-09-10 21:07:49 +1000

    registering a user creates a userprofile entry

--- components/Register.tsx
+++ components/Register.tsx
@@ -1,69 +1,95 @@
 import * as React from 'react';
-import { StyleSheet, Button, Image, Platform } from 'react-native';
+import { StyleSheet, Button, ScrollView, Platform, Picker } from 'react-native';
 import { useState, useEffect, Component } from 'react';
 
 import EditScreenInfo from './EditScreenInfo';
 import { Text, View } from './Themed';
 import * as firebase from 'firebase';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 export default function Register() {
 
-  //Runs on the first launch to get all the needed information for the game
-    //TODO: Need a new default pic to begin OR somehow have a better original state
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [homeLang, setHomeLang] = useState('English');
+    const [forLang, setForLang] = useState('Spanish');
 
     return (
-      <div>
-        <form onSubmit={e => registerUser(e, name, email, password)}>
-        <input name='name' type='name' placeholder='Name' value={name} onChange={e => setName(e.target.value)} required />
-        <input name='email' type='email' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} required />
-        <input name='password' type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} required />
-       
-        <button type='submit'>Register</button>
-        </form>
-      </div>
+      <ScrollView>
+        <Text > Register for MeMa </Text>
+        <View>
+          <TextInput 
+            placeholder="Name" 
+            onChangeText={(t) => setName(t)}
+            value={name}/>
+          <TextInput 
+            placeholder="Email" 
+            onChangeText={(t) => setEmail(t)}
+            value={email}/>
+          <TextInput
+            secureTextEntry={true}
+            placeholder="Password"
+            onChangeText={(t) => setPassword(t)}
+            value={password}/>
+          <Picker
+            selectedValue={homeLang}
+            onValueChange={homeLang => setHomeLang(homeLang)}>
+            <Picker.Item label="English" value="English" />
+            <Picker.Item label="Spanish" value="Spanish" />
+            <Picker.Item label="Chinese" value="Chinese" />
+          </Picker>
+          <Picker
+            selectedValue={forLang}
+            onValueChange={forLang => setForLang(forLang)}>
+            <Picker.Item label="English" value="English" />
+            <Picker.Item label="Spanish" value="Spanish" />
+            <Picker.Item label="Chinese" value="Chinese" />
+          </Picker>
+          <Button title="Register" onPress={event => registerUser(event, name, email, password, forLang, homeLang)} />
+        </View>
+      </ScrollView>
     )
 }
 
-async function registerUser(event, name, email, password){
+async function registerUser(event, name, email, password, forLang, homeLang){
   const auth = firebase.auth();
   const db = firebase.firestore();
+  const usersCollection = "users";
   event.preventDefault();
-  console.log('form submitted');
-  console.log(name);
-  console.log(password);
+  // console.log('form submitted');
+  // console.log(name);
+  // console.log(password);
 
-  auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+  auth.createUserWithEmailAndPassword(email, password).then(function(data){
+    var user = auth.currentUser;
+    if (user != null){
+      user.updateProfile({
+        displayName: name
+      }).then(function() {
+          // console.log(user);
+      });  
+      const userData = {
+        email: user.email,
+        forLang: forLang,
+        homeLang: homeLang, 
+        name: name,
+        userName: name,
+        signedUp: firebase.firestore.Timestamp.now()
+      }
+      const rest = db.collection(usersCollection).doc(user.uid).set(userData);
+    } else {
+      // TODO: send an alert if there is an error 
+    }      
+  }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(errorMessage);
+    console.log(errorMessage, errorCode);
+    // TODO: send an alert if there is an error
   });
-
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      // User logged in already or has just logged in.
-      console.log(user.uid);
-    } else {
-      // User not logged in or has just logged out.
-    }
-  });
-}
-
-
-
-/*
-Makes a dictionary out of all the words in the DB.
-
-@param: dbh - Reference the the firestor DB.
-*/
-async function makeWordURLDict(dbh : firebase.firestore.Firestore) {
-
-}
-  
+} 
 
 
 const styles = StyleSheet.create({
