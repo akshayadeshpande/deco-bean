67e3506dec7aaaa0d42373917bb2b12bcbb4a551
David Riddell <davidriddell27@outlook.com>
2020-09-20 19:03:09 +1000

    working server randomization

--- functions/index.js
+++ functions/index.js
@@ -72,12 +72,7 @@ exports.getMe = functions.https.onCall(async (data, context) => {
 })
 
 
-//Generates a random number that includes the given numbers
-var getRandomIntInclusive = ( (min, max) => {
-min = Math.ceil(min);
-max = Math.floor(max);
-return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-})
+
 
 exports.getRandomWords = functions.https.onCall(async(data, context) => {
   try{
