09154215cc53b408668fcb8d522e39c6d9278ec4
dannyt622 <69776083+dannyt622@users.noreply.github.com>
2020-09-28 11:12:14 +1000

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import * as firebase from 'firebase';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import * as firebase from "firebase";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-K9bYAv1RIsbE29iE9xRHiT_3XyWzwZ0",
@@ -15,15 +15,16 @@ const firebaseConfig = {
   storageBucket: "bean-f1602.appspot.com",
   messagingSenderId: "546478099763",
   appId: "1:546478099763:web:cb43ebe7de34c6a82b0246",
-  measurementId: "G-HC338643HJ"
+  measurementId: "G-HC338643HJ",
 };
 
 firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  
 
   if (!isLoadingComplete) {
     return null;
