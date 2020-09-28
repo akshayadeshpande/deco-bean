083d4cab04c3ccce71906a6b670f2199abdad2a3
dannyt622 <69776083+dannyt622@users.noreply.github.com>
2020-09-28 10:54:14 +1000

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import * as firebase from "firebase";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-K9bYAv1RIsbE29iE9xRHiT_3XyWzwZ0",
+  authDomain: "bean-f1602.firebaseapp.com",
+  databaseURL: "https://bean-f1602.firebaseio.com",
+  projectId: "bean-f1602",
+  storageBucket: "bean-f1602.appspot.com",
+  messagingSenderId: "546478099763",
+  appId: "1:546478099763:web:cb43ebe7de34c6a82b0246",
+  measurementId: "G-HC338643HJ",
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -21,4 +37,5 @@ export default function App() {
     );
   }
 }
- 
+
+export { firebase };
