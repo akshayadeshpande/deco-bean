ac608006ba246b5084945c3a80f9269272a4cf8b
dannyt622 <69776083+dannyt622@users.noreply.github.com>
2020-10-18 21:41:21 +1000

    Latest version of WOTD, base on user forLang

--- components/WOTD.tsx
+++ components/WOTD.tsx
@@ -8,7 +8,6 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 import 'firebase/functions';
 import 'firebase/auth';
-
 /* Word of the Day Component. 
  * Allows us to plug this into a screen.
 */
@@ -16,24 +15,45 @@ import 'firebase/auth';
 /* RENDERING */
 // This function needs to be upper case to be used as a component tag otherwise it thinks it's an html tag.
 export default function WordOfTheday(props) {
-  const [word, setWord] = useState('');
-
+  const [ENword, setENWord] = useState('');
+  const [SPword, setSPWord] = useState('');
+  const [CHword, setCHWord] = useState('');
+  const [forLang, setForLang]= useState('');
+  var myWord;
   useEffect(()=>{
     const getWotd = firebase.functions().httpsCallable('getWotd')
+    const getUser = firebase.functions().httpsCallable('getUser')
+
+    getUser({}).then((result) => {
+      let user = result.data.user
+      console.log(user)
+      setForLang(user.forLang)
+    }).catch(function(err){
+      console.log(err)
+    })
     getWotd({}).then((result) => {
       let wotd = result.data.word
       console.log(wotd)
-      setWord(wotd.EN)
+        setSPWord(wotd.SP)
+        setENWord(wotd.EN)
+        setCHWord(wotd.CH)
     }).catch(function(err){
       console.log(err)
     })
   },[])
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Word of the Day</Text>
-      <Text style={styles.text}>{word}</Text>
-    </View>
-  )
+    if(forLang == "Spanish"){
+      myWord = SPword
+    } else if (forLang == "English"){
+      myWord = ENword
+    } else{
+      myWord = CHword
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Word of the Day</Text>
+        <Text style={styles.text}>{myWord}</Text>
+      </View>
+    )
 }
 
 /* STYLES */
