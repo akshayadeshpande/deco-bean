5ebbf216c06cd1a1f0f78c345cc38ad9b1a9db21
dannyt622 <69776083+dannyt622@users.noreply.github.com>
2020-10-18 20:26:32 +1000

    new WOTD

---
+++ HomeScreen.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Text, View } from '../components/Themed';
+import { getDailyWord } from '../components/WOTD';
+import WordOfTheDay from '../components/WOTD';
+import NavTouchButton from '../components/NavTouchButton';
+import * as firebase from 'firebase';
+
+
+export default function HomeScreen({navigation, props}) {
+ 
+  return (
+    <View style={styles.wrapper}>
+
+      <View style={styles.containerRow}>
+        <View style={styles.navBox}>
+          <NavTouchButton screenName="Profile" text="My Profile" iconName="user"/>
+        </View>
+        <View style={styles.navBox}>
+          <NavTouchButton screenName="Words" text="My Words" iconName="book"/>
+        </View>
+      </View>
+      <View style={styles.containerRow}>
+        <View style={styles.navBox}>
+          <NavTouchButton screenName="Challenge" text="Challenge Mode" iconName="gamepad"/>
+        </View>
+        <View style={styles.navBox}>
+          <NavTouchButton screenName="MeMa" text="Talk to MeMa" iconName="comments-o"/>
+        </View>
+      </View>
+
+      <View style={styles.container}>
+          <WordOfTheDay navigation={navigation} props={props} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  containerRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
