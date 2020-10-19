513080dfc02772b5f124616a497e5eb5854a8f82
dannyt622 <69776083+dannyt622@users.noreply.github.com>
2020-10-19 19:16:25 +1000

    added back change email and text in profile

---
+++ screens/ChangeEmail.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Linking,
+  Alert,
+} from "react-native";
+import { TestComponent, PhoneButton } from "./../components/AppComponents";
+import * as firebase from "firebase";
+
+export default class changeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPassword: "",
+      newPassword: "",
+      newEmail: "",
+    };
+  }
+
+  // Occurs when signout is pressed...
+  onSignoutPress = () => {
+    firebase.auth().signOut();
+  };
+
+  // Reauthenticates the current user and returns a promise...
+  reauthenticate = (currentPassword) => {
+    var user = firebase.auth().currentUser;
+    var cred = firebase.auth.EmailAuthProvider.credential(
+      user.email,
+      currentPassword
+    );
+    return user.reauthenticateWithCredential(cred);
+  };
+
+  // Changes user's password...
+  onChangePasswordPress = () => {
+    this.reauthenticate(this.state.currentPassword)
+      .then(() => {
+        var user = firebase.auth().currentUser;
+        user
+          .updatePassword(this.state.newPassword)
+          .then(() => {
+            alert("Password was changed");
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  // Changes user's email...
+  onChangeEmailPress = () => {
+    this.reauthenticate(this.state.currentPassword)
+      .then(() => {
+        var user = firebase.auth().currentUser;
+        user
+          .updateEmail(this.state.newEmail)
+          .then(() => {
+            alert("Email was changed");
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  render() {
+    return (
+      <ScrollView
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          paddingVertical: 50,
+          paddingHorizontal: 10,
+        }}
+      >
+        <TextInput
+          style={styles.textInput}
+          value={this.state.currentPassword}
+          placeholder="Current Password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            this.setState({ currentPassword: text });
+          }}
+        />
+
+        <TextInput
+          style={styles.textInput}
+          value={this.state.newPassword}
+          placeholder="New Password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            this.setState({ newPassword: text });
+          }}
+        />
+
+        <Button title="Change Password" onPress={this.onChangePasswordPress} />
+
+        <TextInput
+          style={styles.textInput}
+          value={this.state.newEmail}
+          placeholder="New Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          onChangeText={(text) => {
+            this.setState({ newEmail: text });
+          }}
+        />
+
+        <Button title="Change Email" onPress={this.onChangeEmailPress} />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "gray",
+    marginVertical: 20,
+    padding: 10,
+    height: 40,
+    alignSelf: "stretch",
+    fontSize: 18,
+  },
+});
